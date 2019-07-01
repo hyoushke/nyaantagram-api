ebeb34b6de8a1b8241540cc9582562076401329c
hyoushke <hyoushke@gmail.com>
2019-07-02 03:18:16 +0800

    Removed not needed images

--- api/models/mongoConnectionString.js
+++ api/models/mongoConnectionString.js
@@ -1,11 +1,12 @@
 
 const {DB_MONGO_USER, DB_MONGO_PASSWORD, DB_MONGO_HOST, DB_MONGO_DATABASE } = process.env;
-const muri = "mongodb://nyaantagram.localhost.mongo:27017/test";
+//const muri = "mongodb://nyaantagram.localhost.mongo:27017/test";
+const muri = `mongodb+srv://${DB_MONGO_USER}:${DB_MONGO_PASSWORD}@${DB_MONGO_HOST}/${DB_MONGO_DATABASE}?retryWrites=true`;
+
 
 const getConnectionString = ()=> {
     console.log(muri);
     return muri;
-
 }
 
 
